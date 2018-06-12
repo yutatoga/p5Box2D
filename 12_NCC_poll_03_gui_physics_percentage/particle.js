9ccaad0064f1959db5be076721769ad0f5eb883a
yuta-toga <yuta-toga@garage.co.jp>
2018-06-12 15:14:53 +0900

    added 12_NCC_poll_03_gui_physics_percentage/

---
+++ 12_NCC_poll_03_gui_physics_percentage/particle.js
@@ -0,0 +1,92 @@
+// The Nature of Code
+// Daniel Shiffman
+// http://natureofcode.com
+
+// A rectangular box
+
+
+// Constructor
+class Particle {
+  constructor(x, y, r, density, friction, restitution) {
+
+    this.r = r;
+    this.density = density || 1.0;
+    this.friction = friction || 0.1;
+    this.restitution = restitution || 0.5;
+
+    // Define a body
+    let bd = new box2d.b2BodyDef();
+    bd.type = box2d.b2BodyType.b2_dynamicBody;
+    bd.position = scaleToWorld(x, y);
+
+    // Define a fixture
+    let fd = new box2d.b2FixtureDef();
+    // Fixture holds shape
+    fd.shape = new box2d.b2CircleShape();
+    fd.shape.m_radius = scaleToWorld(this.r);
+
+    // Some physics
+    fd.density = this.density;
+    fd.friction = this.friction;
+    fd.restitution = this.restitution;
+
+    // Create the body
+    this.body = world.CreateBody(bd);
+    // Attach the fixture
+    this.body.CreateFixture(fd);
+
+    // Some additional stuff
+    // initial force
+    this.body.SetLinearVelocity(new box2d.b2Vec2(random(-5, 5), random(30, 50)));
+    this.body.SetAngularVelocity(random(-5, 5));
+  }
+
+  // This function removes the particle from the box2d world
+  killBody() {
+    world.DestroyBody(this.body);
+  }
+
+  // Is the particle ready for deletion?
+  done() {
+    // Let's find the screen position of the particle
+    let transform = this.body.GetTransform();
+    let pos = scaleToPixels(transform.position);
+    // Is it off the bottom of the screen?
+    if (pos.y > height + this.r * 2) {
+      this.killBody();
+      return true;
+    }
+    return false;
+  }
+
+  // Drawing the Particle
+  display(enableDebug) {
+    // Get the body's position
+    let pos = scaleToPixels(this.body.GetPosition());
+    // Get its angle of rotation
+    let a = this.body.GetAngleRadians();
+
+    // Draw it!
+    rectMode(CENTER);
+    push();
+    translate(pos.x, pos.y);
+    rotate(a);
+
+    if(pos.x < width/2){
+      fill(255, 20, 160);
+    }else{
+      fill(60, 80, 230);
+    }
+
+    if(enableDebug){
+      stroke(255);
+      strokeWeight(1);
+    } else {
+      strokeWeight(0);
+    }
+    ellipse(0, 0, this.r * 2, this.r * 2);
+    // Let's add a line so we can see the rotation
+    line(0, 0, this.r, 0);
+    pop();
+  }
+}
---
+++ 12_NCC_poll_03_gui_physics_percentage/sketch.js
@@ -0,0 +1,112 @@
+var Settings = function() {
+  this.density = 3.0;
+  this.friction = 0.1;
+  this.restitution = 0.53;
+  this.enableDebug = true;
+
+  this.surfaceWidth = 180;
+
+  this.ratioLeft = 0.3;
+  this.paritcleAddSpeed = 0.5; // 0~1: 1 is maximum speed
+
+  this.drop = function(){
+    // reset volumes
+    volumeRight = 0;
+    volumeLeft = 0;
+
+    // destroy all particles from world
+    for (var i = 0; i < particles.length; i++){
+      particles[i].killBody();
+    }
+
+    // clear particles array
+    particles.length = 0;
+  };
+}
+
+
+// A reference to our box2d world
+let world;
+
+// A list for all of our particles
+let particles = [];
+
+// An object to store information about the uneven surface
+let surface;
+
+var settings = new Settings();
+
+var volumeLeft = 0;
+var volumeRight = 0;
+var volumeMagicNumber = 1.25;
+
+function setup() {
+  createCanvas(960, 540);
+
+  // Initialize box2d physics and create the world
+  world = createWorld();
+
+  // Create the surface
+
+  surface = new Surface(settings.surfaceWidth);
+
+  var gui = new dat.GUI();
+  gui.add(settings, 'drop');
+  gui.add(settings, 'ratioLeft', 0, 1);
+  gui.add(settings, 'density', 0, 100);
+  gui.add(settings, 'friction', 0, 100);
+  gui.add(settings, 'restitution', 0, 1);
+  gui.add(settings, 'enableDebug');
+}
+
+function draw() {
+  background(0);
+
+  // We must always step through time!
+  let timeStep = 1.0 / 60;
+  // 2nd and 3rd arguments are velocity and position iterations
+  world.Step(timeStep, 40, 20);
+
+  // particles fall from the top every so often
+
+  // LEFT
+  if (random(1) < settings.paritcleAddSpeed && (volumeLeft * volumeMagicNumber) / (settings.surfaceWidth * height) <  settings.ratioLeft) {
+    let radius = random(5, 30);
+    volumeLeft += radius * radius * Math.PI;
+
+    let positionX = random(radius, settings.surfaceWidth - radius);
+    let positionY = random(-settings.surfaceWidth, -radius);
+    particles.push(new Particle(positionX, positionY, radius, settings.density, settings.friction, settings.restitution));
+  }
+
+  // RIGHT
+  if (random(1) < settings.paritcleAddSpeed && (volumeRight * volumeMagicNumber) / (settings.surfaceWidth * height) < (1 - settings.ratioLeft)) {
+    let radius = random(5, 30);
+    volumeRight += radius * radius * Math.PI;
+
+    let positionX = random(width - settings.surfaceWidth + radius, width - radius);
+    let positionY = random(-settings.surfaceWidth, -radius);
+    particles.push(new Particle(positionX, positionY, radius, settings.density, settings.friction, settings.restitution));
+  }
+
+  // Display all the particles
+  for (let i = particles.length - 1; i >= 0; i--) {
+    particles[i].display(settings.enableDebug);
+    if (particles[i].done()) {
+      particles.splice(i, 1);
+    }
+  }
+
+  // Debug
+  if(settings.enableDebug){
+    // Draw the surface
+    surface.display();
+
+    // Draw the guide line for the ratio
+    // - left
+    stroke(0, 255, 255);
+    line(0, height * (1 - settings.ratioLeft), settings.surfaceWidth, height * (1 - settings.ratioLeft));
+    line(width - settings.surfaceWidth, height * settings.ratioLeft, width, height * settings.ratioLeft);
+  }
+
+}
